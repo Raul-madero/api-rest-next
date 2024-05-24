98168e7d9aa3843e02cb5ad9650ffb22841ce5f8
Raul Madero <r.madero.ramirez@gmail.com>
2024-05-23 19:56:22 -0600

    primera accion de servidor

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { prisma } from '../../../libs/prismadb'
 
-const ToDoPage = () => {
+const ToDoPage = async () => {
+
+    const todos = await prisma.todo.findMany()
+    console.log(todos)
+
+    const createTodo = async () => {
+        'use server'
+        console.log("server action")
+    }
+
   return (
     <div>
-      Todo Page
+      <h1 className="text-center text-3xl">Todos</h1>
+      <form action={createTodo}>
+        <input type="text" name='title' className='border rounded-md border-gray-500 mx-10' />
+        <button type='submit' className='border rounded-lg border-gray-500 mr-2 p-2'>Submit</button>
+      </form>
+      <pre>
+        {JSON.stringify(todos, null, 2)}
+      </pre>
     </div>
   )
 }
